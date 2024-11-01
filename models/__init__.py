bddba11d75bbb4ad4634dd02522e12ac6e97e7a3
Salokled <nikolasmartins@hotmail.com>
2024-11-01 18:39:56 -0300

    Commit final

---
+++ models/__init__.py
@@ -0,0 +1,11 @@
+from flask_sqlalchemy import SQLAlchemy
+
+# Cria a instância do SQLAlchemy
+db = SQLAlchemy()
+
+# Importa os modelos (Produto e Usuario) após a criação do db
+from .produto import Produto
+from .usuario import Usuario
+from .cliente import Cliente
+from .pedido import Pedido
+from .detalhe_pedido import DetalhePedido
